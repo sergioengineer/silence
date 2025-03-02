1686dfb3c464795870c04b0b9603e0e6be35d624
SERGIO BRAGA <sergiobg.engineer@gmail.com>
2025-03-02 09:15:00 -0300

    feat: raylib building

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,11 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("defines.h");
-    @cInclude("math.h");
     @cInclude("raylib-nuklear.h");
 });
 
 pub fn main() !void {
     c.InitWindow(640, 480, "raylib example");
-    const fontSize = 14;
-    _ = c.InitNuklear(fontSize);
 
     while (c.WindowShouldClose() == false) {
         c.BeginDrawing();
